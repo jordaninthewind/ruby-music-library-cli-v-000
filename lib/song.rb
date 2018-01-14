4699f7e7471868e0f4b242638189e4f941dd1892
Jordan Kline <jordan.kline@gmail.com>
2018-01-14 17:08:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,8 +52,9 @@ class Song
     self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
   end
 
-  # def self.new_from_filename(filename)
-  #   name, title = filename.split(" - ")
-  #   self.find_or_create_by_name(title)
-  # end
+  def self.new_from_filename(filename)
+    name, title = filename.split(" - ")
+    self.find_or_create_by_name(title)
+    binding.pry
+  end
 end
