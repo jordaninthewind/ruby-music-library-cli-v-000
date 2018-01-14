0b7f2a1b9e84170ea21b49323dd704382e6303af
Jordan Kline <jordan.kline@gmail.com>
2018-01-14 18:30:08 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -66,7 +66,8 @@ attr_reader :path
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist = gets.chomp
-    x = Artist.find_by_name(artist).songs#.sort_by {|el| el.name}
+    x = Artist.find_by_name(artist)
+    # x.songs#.sort_by {|el| el.name}
     binding.pry
   end
 
