7aa336e0d715e02f1b3b618e2db2261aaf1a27ef
Jordan Kline <jordan.kline@gmail.com>
2018-01-14 18:15:05 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -49,9 +49,9 @@ attr_reader :path
 
   def list_artists
     x = Artist.all.sort_by { |i| i.name }.uniq
-    # binding.pry
+    binding.pry
     x.each_with_index do |el, i|
-      puts "#{i + 1}. #{el.artist.name}"
+      puts "#{i + 1}. #{el.name}"
     end
   end
 
