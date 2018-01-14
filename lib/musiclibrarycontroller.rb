e164e38bdb7768a988601fad080a9f1990174caa
Jordan Kline <jordan.kline@gmail.com>
2018-01-14 18:47:38 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -80,7 +80,7 @@ attr_reader :path
     x = Genre.find_by_name(genre)
     if x != nil
       # binding.pry
-      x.songs.sort_by {|el| el.name}.each_with_index do |el, i|
+      x.songs.sort_by {|el| el.artist.name }.each_with_index do |el, i|
         binding.pry
         # puts "#{i + 1}. #{el.name} - #{el.genre.name}"
       end
